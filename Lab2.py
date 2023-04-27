c3f1744a2b51d553ee4d06c6104ed82f2eb58b20
AaronLea <aaronlea.22@ichat.sp.edu.sg>
2023-04-27 11:50:51 +0800

    Modified Lab2.py to calculate median

--- Lab2.py
+++ Lab2.py
@@ -1,12 +1,12 @@
-from  statistics import *
-
 def main():
     print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
     display_main_menu()
     num_list = get_user_input()
     calc_average(num_list)
     find_min_max(num_list)
-    sort_temperature(num_list)
+    sortednum_list = sort_temperature(num_list)
+    calc_median_temperature(sortednum_list)
+
 def display_main_menu():
     print("Enter some numbers separated by commas (e.g. 5, 67, 32)")
 def get_user_input():
@@ -35,6 +35,18 @@ def sort_temperature(numberslist):
 
     #print(sortednumberslist)
     return sortednumberslist
+
+def calc_median_temperature(sortednumberslist):
+    middleindex = int(len(sortednumberslist) / 2)
+
+    if (len(sortednumberslist) % 2 == 0):
+        secondmiddleindex = middleindex - 1
+        median = (sortednumberslist[middleindex] + sortednumberslist[secondmiddleindex]) / 2
+    else:
+        median = (sortednumberslist[middleindex])
+
+    #print(median)
+    return(median)
 
 if __name__ == "__main__":
     main()
