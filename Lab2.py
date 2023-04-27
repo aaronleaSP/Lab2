21e5da092938cc00d86ca1a3674f0e530e10d4a3
AaronLea <aaronlea.22@ichat.sp.edu.sg>
2023-04-27 11:37:16 +0800

    Modified Lab2.py for Exercise 3 and 4

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,40 @@
-print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+from  statistics import *
+
+def main():
+    print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+    display_main_menu()
+    num_list = get_user_input()
+    calc_average(num_list)
+    find_min_max(num_list)
+    sort_temperature(num_list)
+def display_main_menu():
+    print("Enter some numbers separated by commas (e.g. 5, 67, 32)")
+def get_user_input():
+    numbers = input("Enter your values: ")
+    numberslist = numbers.split(", ")
+    temp = []
+    for number in numberslist:
+        temp += [float(number)]
+
+    numberslist = temp
+
+    #print(numberslist)
+    return numberslist
+def calc_average(numberslist):
+    average = sum(numberslist) / len(numberslist)
+
+    #print(average)
+    return average
+def find_min_max(numberslist):
+    minmaxtemp = [int(min(numberslist)), int(max(numberslist))]
+
+    #print(minmaxtemp)
+    return minmaxtemp
+def sort_temperature(numberslist):
+    sortednumberslist = sorted(numberslist)
+
+    #print(sortednumberslist)
+    return sortednumberslist
+
+if __name__ == "__main__":
+    main()
