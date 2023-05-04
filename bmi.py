f668dfc3aea1e1fa53ddf6f231e4da21327e5f8f
AaronLea <aaronlea.22@ichat.sp.edu.sg>
2023-05-04 10:23:45 +0800

    Modify bmi.py to return values

--- bmi.py
+++ bmi.py
@@ -8,10 +8,13 @@ def calculate_bmi(height, weight):
 
     if bmi < 18.5:
         print("Weight Classification: Under Weight")
+        return -1
     elif 18.5 <= bmi <= 25.0:
         print("Weight Classification: Normal Weight")
+        return 0
     else:
         print("Over Weight")
+        return 1
 
 calculate_bmi(weight=57, height=1.73)
 
